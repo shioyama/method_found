76b3873c9c513cba7cac1af6ff582a8d8296fb1e
Chris Salzberg <chris@dejimata.com>
2017-04-18 08:48:01 +0900

    Refactor define methods into their own methods

--- lib/method_found/interceptor.rb
+++ lib/method_found/interceptor.rb
@@ -1,6 +1,14 @@
 module MethodFound
   class Interceptor < Module
-    def initialize(regex, &intercept_block)
+    attr_reader :regex
+
+    def initialize(regex = nil, &intercept_block)
+      define_method_missing(regex, &intercept_block) unless regex.nil?
+      define_inspect
+    end
+
+    def define_method_missing(regex, &intercept_block)
+      @regex = regex
       define_method :method_missing do |method_name, *arguments, &method_block|
         if matches = regex.match(method_name)
           instance_exec(method_name, matches, *arguments, &intercept_block)
@@ -12,7 +20,10 @@ module MethodFound
       define_method :respond_to_missing? do |method_name, include_private = false|
         (method_name =~ regex) || super(method_name, include_private)
       end
+    end
 
+    def define_inspect
+      regex = @regex
       define_method :inspect do
         klass = self.class
         name  = klass.name || klass.inspect
