9eed9251468fb8ada52ce85e9a6eb7cb74cef844
Chris Salzberg <chris@dejimata.com>
2017-04-21 20:01:51 +0900

    Update Guardfile + RSpec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,11 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'method_found'
 
 require 'pry-byebug'
+
+RSpec.configure do |config|
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+end
