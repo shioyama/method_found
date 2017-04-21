a5c3f3b22cbbe6a55e9fed72f32fdd39ce6ca8a2
Chris Salzberg <chris@dejimata.com>
2017-04-21 20:13:23 +0900

    Use abstract matcher rather than regex

--- lib/method_found/interceptor.rb
+++ lib/method_found/interceptor.rb
@@ -1,18 +1,18 @@
 module MethodFound
   class Interceptor < Module
-    attr_reader :regex
+    attr_reader :matcher
 
     def initialize(regex = nil, &intercept_block)
       define_method_missing(regex, &intercept_block) unless regex.nil?
     end
 
-    def define_method_missing(regex, &intercept_block)
-      @regex = regex
+    def define_method_missing(matcher, &intercept_block)
+      @matcher = matcher
       intercept_method = assign_intercept_method(&intercept_block)
       method_cacher    = method(:cache_method)
 
       define_method :method_missing do |method_name, *arguments, &method_block|
-        if matches = regex.match(method_name)
+        if matches = matcher.match(method_name)
           method_cacher.(method_name, matches)
           send(method_name, *arguments, &method_block)
         else
@@ -21,14 +21,14 @@ module MethodFound
       end
 
       define_method :respond_to_missing? do |method_name, include_private = false|
-        (method_name =~ regex) || super(method_name, include_private)
+        matcher.match(method_name) || super(method_name, include_private)
       end
     end
 
     def inspect
       klass = self.class
       name  = klass.name || klass.inspect
-      "#<#{name}: #{regex.inspect}>"
+      "#<#{name}: #{matcher.inspect}>"
     end
 
     private
