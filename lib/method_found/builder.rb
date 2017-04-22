c6a98183aeb323926e1db93819d78364f67bff16
Chris Salzberg <chris@dejimata.com>
2017-04-22 17:19:47 +0900

    Keep reference to interceptors in builder

--- lib/method_found/builder.rb
+++ lib/method_found/builder.rb
@@ -1,11 +1,15 @@
 module MethodFound
   class Builder < Module
+    attr_reader :interceptors
+
     def initialize(&block)
+      @interceptors = []
       instance_eval &block
     end
 
     def intercept(*args, &block)
-      include Interceptor.new(*args, &block)
+      @interceptors.push(interceptor = Interceptor.new(*args, &block))
+      include interceptor
     end
   end
 end
