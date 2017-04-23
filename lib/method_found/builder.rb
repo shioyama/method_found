bdd560496983efe933e6ade15b01facab67230fc
Chris Salzberg <chris@dejimata.com>
2017-04-23 11:23:32 +0900

    Add docs for MethodFound::Interceptor and MethodFound::Builder

--- lib/method_found/builder.rb
+++ lib/method_found/builder.rb
@@ -1,7 +1,32 @@
 module MethodFound
+=begin
+
+Creates set of interceptors to include into a class.
+
+@example
+  class Post
+    builder = MethodFound::Builder.new {
+      intercept /\Asay_([a-z]+)\Z/ do |method_name, matches, *arguments|
+        "#{matches[1]}!"
+      end
+
+      intercept /\Ayell_([a-z]+)\Z/ do |method_name, matches, *arguments|
+        "#{matches[1]}!!!"
+      end
+    }
+  end
+
+  foo = Foo.new
+  foo.say_hello
+  #=> "hello!"
+  foo.yell_hello
+  #=> "hello!!!"
+=end
   class Builder < Module
     attr_reader :interceptors
 
+    # @yield Yields builder as context to block, to allow calling builder
+    #   methods to create interceptors in included class.
     def initialize(&block)
       @interceptors = []
       instance_eval &block
--- lib/method_found/interceptor.rb
+++ lib/method_found/interceptor.rb
@@ -1,18 +1,33 @@
 module MethodFound
+=begin
+
+Class for intercepting method calls using method_missing. Initialized by
+passing in a matcher object which can be a Regexp, a proc or lambda, or a
+string/symbol.
+
+=end
   class Interceptor < Module
     attr_reader :matcher
 
+    # Creates an interceptor module to include into a class.
+    # @param (see #define_method_missing)
     def initialize(matcher = nil, &intercept_block)
       define_method_missing(matcher, &intercept_block) unless matcher.nil?
     end
 
-    def define_method_missing(matcher_, &intercept_block)
-      @matcher = matcher = Matcher.new(matcher_)
+    # Define method_missing and respond_to_missing? on this interceptor. Can be
+    # called after interceptor has been created.
+    # @param [Regexp,Proc,String,Symbol] matcher Matcher for intercepting
+    #   method calls.
+    # @yield [method_name, matches, &block] Yiels method_name matched, set of
+    #   matches returned from matcher, and block passed to method when called.
+    def define_method_missing(matcher, &intercept_block)
+      @matcher = matcher_ = Matcher.new(matcher)
       assign_intercept_method(&intercept_block)
       method_cacher = method(:cache_method)
 
       define_method :method_missing do |method_name, *arguments, &method_block|
-        if matches = matcher.match(method_name)
+        if matches = matcher_.match(method_name)
           method_cacher.(method_name, matches)
           send(method_name, *arguments, &method_block)
         else
@@ -21,7 +36,7 @@ module MethodFound
       end
 
       define_method :respond_to_missing? do |method_name, include_private = false|
-        if matches = matcher.match(method_name)
+        if matches = matcher_.match(method_name)
           method_cacher.(method_name, matches)
         else
           super(method_name, include_private)
