ae59ff478e45c503f56043bffac74343650cb26d
Chris Salzberg <chris@dejimata.com>
2017-04-23 11:38:58 +0900

    Add documentation for SimpleModel

--- lib/method_found/simple_model.rb
+++ lib/method_found/simple_model.rb
@@ -1,9 +1,63 @@
 require "method_found"
 
 module MethodFound
+=begin
+
+Example class using MethodFound interceptors to implement attributes and change
+tracking. Symbols passed into constructor define patterns in interceptor
+modules to catch method calls and define methods on module as needed. New
+methods can be dynamically defined by calling any undefined method with a bang (!).
+
+This class is not included by default when requiring MethodFound, so you will need to explicitly require it with:
+
+  require "method_found/simple_model"
+
+@example Setting Attributes
+  post = MethodFound::SimpleModel.new(:title, :content)
+
+  post.title = "Method Found"
+  post.content = "Once upon a time..."
+
+  post.title
+  #=> "Method Found"
+
+  post.content
+  #=> "Once upon a time..."
+
+@example Dyanamically defining new attributes
+  post = MethodFound::SimpleModel.new
+
+  post.foo
+  #=> raises MethodNotFound error
+
+  post.foo!
+  #=> nil
+
+  post.foo = "my foo"
+  post.foo
+  #=> "my foo"
+
+@example Tracking changes
+  post = MethodFound::SimpleModel.new(:title)
+
+  post.title = "foo"
+  post.title_changed?
+  #=> false
+
+  post.title = "bar"
+  post.title_changed?
+  #=> true
+  post.title_was
+  #=> "foo"
+  post.title_changes
+  #=> ["bar", "foo"]
+
+=end
   class SimpleModel
     attr_reader :attribute_builder
 
+    # @param attribute_names [Symbol] One or more attribute names to define
+    #   interceptors for on model.
     def initialize(*attribute_names)
       @attributes = Hash.new { |h, k| h[k] = [] }
 
