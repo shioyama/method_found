cd56019a28fd19119af19317d6a0b1811c6f03c8
Chris Salzberg <chris@dejimata.com>
2017-04-26 23:33:59 +0900

    Add AttributeMethods

---
+++ lib/method_found/attribute_methods.rb
@@ -0,0 +1,69 @@
+require "method_found/attribute_interceptor"
+
+module MethodFound
+=begin
+
+@example
+  class Person < Struct.new(:attributes)
+    include MethodFound::AttributeMethods
+
+    attribute_method_suffix ''
+    attribute_method_suffix '='
+    attribute_method_suffix '_contrived?'
+    attribute_method_prefix 'clear_'
+    attribute_method_affix  prefix: 'reset_', suffix: '_to_default!'
+
+    define_attribute_methods :name
+
+    def initialize(attributes = {})
+      super(attributes)
+    end
+
+    private
+
+    def attribute_contrived?(attr)
+      true
+    end
+
+    def clear_attribute(attr)
+      send("#{attr}=", nil)
+    end
+
+    def reset_attribute_to_default!(attr)
+      send("#{attr}=", 'Default Name')
+    end
+
+    def attribute(attr)
+      attributes[attr]
+    end
+
+    def attribute=(attr, value)
+      attributes[attr] = value
+    end
+  end
+
+=end
+  module AttributeMethods
+    def self.included(base)
+      base.instance_eval do
+        def attribute_method_affix(prefix: '', suffix: '')
+          include(AttributeInterceptor.new(prefix: prefix, suffix: suffix))
+        end
+
+        def attribute_method_suffix(suffix)
+          include(AttributeInterceptor.new(suffix: suffix))
+        end
+
+        def attribute_method_prefix(prefix)
+          include(AttributeInterceptor.new(prefix: prefix))
+        end
+
+        def define_attribute_methods(*attributes)
+          ancestors.each do |ancestor|
+            ancestor.define_attribute_methods(*attributes) if ancestor.is_a?(AttributeInterceptor)
+          end
+        end
+      end
+    end
+  end
+end
